679c2d44d0681e2399a41e81e201091a1625d515
Patrick José Pereira <patrickelectric@gmail.com>
2020-01-10 09:09:05 -0300

    build: Remove flags variable
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- build.rs
+++ build.rs
@@ -3,9 +3,6 @@ extern crate vergen;
 use vergen::{ConstantsFlags, generate_cargo_keys};
 
 fn main() {
-    // Setup the flags, toggling off the 'SEMVER_FROM_CARGO_PKG' flag
-    let mut flags = ConstantsFlags::all();
-
     // Generate the 'cargo:' key output
     generate_cargo_keys(ConstantsFlags::all()).expect("Unable to generate the cargo keys!");
 }
