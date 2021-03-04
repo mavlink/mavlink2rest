6e70a8062aa1fb3a684ad617b84059ada1d9c1ee
Patrick José Pereira <patrickelectric@gmail.com>
2021-03-03 22:49:48 -0300

    build: Add vue download
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
+extern crate reqwest;
 extern crate vergen;
 
 use vergen::{generate_cargo_keys, ConstantsFlags};
@@ -5,4 +6,13 @@ use vergen::{generate_cargo_keys, ConstantsFlags};
 fn main() {
     // Generate the 'cargo:' key output
     generate_cargo_keys(ConstantsFlags::all()).expect("Unable to generate the cargo keys!");
+
+    // Download vue file
+    let vue_url = "https://unpkg.com/vue@3.0.5/dist/vue.global.js";
+    let mut resp = reqwest::blocking::get(vue_url)
+        .expect(&format!("Failed to download vue file: {}", &vue_url));
+    let vue_file_path = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("src/html/vue.js");
+    let mut output_file = std::fs::File::create(&vue_file_path)
+        .expect(&format!("Failed to create vue file: {:?}", &vue_file_path));
+    std::io::copy(&mut resp, &mut output_file).expect("Failed to copy content.");
 }
