fd631a182a29f09557234783880910e7c81d1cbf
Patrick José Pereira <patrickelectric@gmail.com>
2021-03-04 08:23:08 -0300

    tests: Fix download link of log file
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- tests/download_log_files.sh
+++ tests/download_log_files.sh
@@ -1,5 +1,5 @@
 if [ ! -d "/tmp/testlogs" ]; then
     echo "log files not found, downloading..."
     mkdir /tmp/testlogs
-    wget http://autotest.ardupilot.org/ArduSub-test.tlog --directory-prefix=/tmp/testlogs/
+    wget https://autotest.ardupilot.org/history/2021-03-03-23:03/ArduSub-test.tlog --directory-prefix=/tmp/testlogs/
 fi
