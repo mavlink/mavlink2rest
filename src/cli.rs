0e103bc6cc524f7379157ab4a73c8555b3c266aa
Patrick José Pereira <patrickelectric@gmail.com>
2021-05-06 10:57:28 -0300

    cli: Remove redundant code
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/cli.rs
+++ src/cli.rs
@@ -110,6 +110,5 @@ mod tests {
         assert_eq!(mavlink_connection_string(), "udpin:0.0.0.0:14550");
         assert_eq!(server_address(), "0.0.0.0:8088");
         assert_eq!(mavlink_version(), 2);
-        assert_eq!(is_verbose(), false);
     }
 }
