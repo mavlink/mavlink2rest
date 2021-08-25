e2bb2f611b456d67994915c146ce338a9fff4454
Patrick José Pereira <patrickelectric@gmail.com>
2021-08-25 12:57:24 -0400

    tests: Update download link
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- tests/download_log_files.sh
+++ tests/download_log_files.sh
@@ -1,5 +1,5 @@
 if [ ! -d "/tmp/testlogs" ]; then
     echo "log files not found, downloading..."
     mkdir /tmp/testlogs
-    wget https://autotest.ardupilot.org/history/2021-03-03-23:03/ArduSub-test.tlog --directory-prefix=/tmp/testlogs/
+    wget https://autotest.ardupilot.org/history/2021-08-24-22:08/ArduSub-test.tlog --directory-prefix=/tmp/testlogs/
 fi
