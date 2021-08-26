fe463d277c0756d5ab13fb1ff55160fa1c715e41
Patrick José Pereira <patrickelectric@gmail.com>
2021-08-26 13:21:24 -0400

    Remove tests for unstable test source files
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- tests/download_log_files.sh
+++
@@ -1,5 +0,0 @@
-if [ ! -d "/tmp/testlogs" ]; then
-    echo "log files not found, downloading..."
-    mkdir /tmp/testlogs
-    wget https://autotest.ardupilot.org/history/2021-08-24-22:08/ArduSub-test.tlog --directory-prefix=/tmp/testlogs/
-fi
