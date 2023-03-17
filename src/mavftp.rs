08bab9cc7c1bc71db528f06dd5e53fca93053cc1
Patrick José Pereira <patrickelectric@gmail.com>
2023-03-17 00:40:18 -0300

    wip
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ src/mavftp.rs
@@ -0,0 +1,94 @@
+use mavlink::MavConnection;
+use std::io::Read;
+use std::str;
+
+fn main() {
+    let target_system = 1; // Replace with the target system ID
+    let target_component = 0; // Replace with the target component ID
+
+    let url = "udpout:192.168.0.46:14660";
+    let mut conn = mavlink::connect(&url).unwrap();
+    conn.set_protocol_version(mavlink::MavlinkVersion::V2);
+
+    let mut payload = [0; 249];
+
+    let mut header = mavlink::MavHeader::default();
+    header.system_id = 1;
+    header.component_id = 0;
+
+    // Set the appropriate payload for the "List" operation
+    let seq_number: u16 = 1;
+    payload[0..2].copy_from_slice(&seq_number.to_le_bytes()); // Sequence number
+    payload[2] = 0; // Session ID
+    payload[3] = 3; // OpCode: 3 for "ListDirectory"
+    payload[4] = 1; // OpCode: 3 for "ListDirectory"
+    payload[8..12].copy_from_slice(&0u32.to_le_bytes()); // Directory offset
+    payload[12..14].copy_from_slice(b"/\0"); // Directory path to list files from
+
+    let msg = mavlink::common::MavMessage::FILE_TRANSFER_PROTOCOL(
+        mavlink::common::FILE_TRANSFER_PROTOCOL_DATA {
+            target_network: 0,
+            target_system,
+            target_component,
+            payload: payload.into(),
+        },
+    );
+
+    let mut buf = [0; 300];
+
+    dbg!("send");
+    conn.send(&header, &msg).expect("Failed to send message");
+    dbg!("loop");
+    loop {
+        // dbg!("recv");
+        let (header, message) = conn.recv().expect("Failed to receive message");
+        // println!("Received: {:?}", message);
+
+        if let mavlink::common::MavMessage::FILE_TRANSFER_PROTOCOL(msg) = message {
+            let payload = msg.payload;
+            let opcode = payload[3];
+            dbg!(&opcode);
+
+            // Check if the received message is an ACK (0x80)
+            if opcode == 0x80 {
+                // Extract and print the list of files
+                let file_list: Vec<String> = str::from_utf8(&payload[12..])
+                    .unwrap_or("")
+                    .split("\0")
+                    .map(Into::into)
+                    .collect();
+                let file_list: Vec<&String> =
+                    file_list.iter().filter(|&name| !name.is_empty()).collect();
+                println!("List of files:\n{:?}", file_list);
+
+                break;
+            } else if opcode == 0x81 {
+                // NAK response
+                let error_code = payload[4];
+                let error_description = match error_code {
+                    0 => "None",
+                    1 => "Fail",
+                    2 => {
+                        let errno = payload[5];
+                        println!("FailErrno with error number: {}", errno);
+                        "FailErrno"
+                    }
+                    3 => "InvalidDataSize",
+                    4 => "InvalidSession",
+                    5 => "NoSessionsAvailable",
+                    6 => "EOF",
+                    7 => "UnknownCommand",
+                    8 => "FileExists",
+                    9 => "FileProtected",
+                    10 => "FileNotFound",
+                    _ => "Unknown error",
+                };
+                println!(
+                    "Received NAK with error code {}: {}",
+                    error_code, error_description
+                );
+                break;
+            }
+        }
+    }
+}
